dbcc4b3ddcf3a54bad8a39210e91251f0c2d590a
Dhruv Vyas <dhruv.m.vyas@gmail.com>
2014-11-29 15:34:23 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing msp tools..."
+sudo apt-get -y -q install msp430mcu msp430-libc mspdebug gcc-msp430 gdb-msp430
+echo "Ok."
+echo "Installing nesc dependancies.."
+sudo apt-get -y -q automake autoconf emacs gperf bison flex
+echo "Ok"
+echo "Downloading necs github source."
+mkdir ~/github/
+cd ~/github/
+git clone https://github.com/tinyos/nesc
+echo "Installing nesc"
+cd nesc
+./Bootstrap && ./configure && make && sudo make install
+cd ..
+echo "Downloading tinyos-main source"
+git clone https://github.com/tinyos/tinyos-main
+echo "Installing tinyos"
+cd tinyos-main/tools
+./Bootstrap && ./configure && make && sudo make install
+echo "Installation done."
