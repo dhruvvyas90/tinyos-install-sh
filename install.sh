f5b9ed0850abd20776e47429f2d1e975fe94aedd
Lu <you@example.com>
2016-01-02 22:14:00 -0500

    add install

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ echo "Installing msp tools..."
 sudo apt-get -y -q install msp430mcu msp430-libc mspdebug gcc-msp430 gdb-msp430
 echo "Ok."
 echo "Installing avr tools..."
-sudo apt-get -y -q gcc-avr binutils-avr gdb-avr avr-libc avrdude
+sudo apt-get -y -q install gcc-avr binutils-avr gdb-avr avr-libc avrdude
 echo "Ok."
 echo "Installing nesc dependancies.."
-sudo apt-get -y -q automake autoconf emacs gperf bison flex git
+sudo apt-get -y -q install automake autoconf emacs gperf bison flex git
 echo "Ok"
 echo "Downloading nesc github source."
 mkdir ~/github/
